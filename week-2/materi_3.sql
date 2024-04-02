bb5c38aede2f7ad17407580dae1383e46acd63be
Hamizan Hibatullah <62762617+ham407@users.noreply.github.com>
2024-04-02 16:20:23 +0700

    Add files via upload

---
+++ week-2/materi_3.sql
@@ -0,0 +1,40 @@
+CREATE TABLE customers(
+	id_customer INT,
+    nama_customer VARCHAR(50),
+    kota VARCHAR(50),
+    PRIMARY KEY(id_customer)
+);
+
+INSERT INTO customers VALUES (1123, 'Reihan', 'Jakarta Barat'),
+							 (1126, 'Abra', 'Depok'),
+                             (1147, 'Beni', 'Jakarta Selatan'),
+                             (1156, 'Lufi', 'Jakarta Selatan'),
+                             (1189, 'Kaido', 'Depok');
+
+CREATE TABLE payments(
+	id_customer INT,
+    id_transaksi INT,
+    tgl_transaksi DATE,
+    jumlah_pembayaran INT,
+    PRIMARY KEY(id_transaksi)
+);
+
+INSERT INTO payments VALUES (1123, 1, '2021-09-01', 500000),
+							(1147, 2, '2021-09-01', 247000),
+                            (1147, 3, '2021-09-02', 325000),
+                            (1189, 4, '2021-09-03', 440000),
+                            (1176, 5, '2021-09-03', 122000);
+
+CREATE TABLE customers2(
+	id_customer INT,
+    nama_customer VARCHAR(50),
+    kota VARCHAR(50),
+    email VARCHAR(50),
+    PRIMARY KEY(id_customer)
+);
+
+INSERT INTO customers2 VALUES (1123, 'Reihan', 'Jakarta Barat', 'reihan@gmail.com'),
+							 (1126, 'Abra', 'Depok', 'abra@yahoo.com'),
+                             (1147, 'Beni', 'Jakarta Selatan', 'beni@gmail.com'),
+                             (1156, 'Lufi', 'Jakarta Selatan', 'lufi@ymail.com'),
+                             (1189, 'Kaido', 'Depok', 'kaido@gmail.com');
